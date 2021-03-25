8f329653d8856fbac21886f0b96178f221d6dabc
Geoffrey Hil <g.hill@sae.edu>
2021-03-25 12:44:38 +1100

    Added Steam.VR haptic code example.

---
+++ Assets/GHSteamvr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR; //add Valve's SteamVR system.
+
+public class GHSteamvr : MonoBehaviour
+{
+    public GHColorChange cube;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        // check steam vr for a input action, in this case InteractUI which is the trigger
+        if (SteamVR_Actions._default.InteractUI.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            cube.SetGreen();
+
+        } else
+        {
+            cube.SetRed();
+        }
+
+
+    }
+}
---
+++ Assets/GHSteamvrHaptics.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class GHSteamvrHaptics : MonoBehaviour
+{
+    public SteamVR_Action_Vibration hapticAction;
+    public SteamVR_Action_Boolean trackpadAction;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trackpadAction.GetStateDown(SteamVR_Input_Sources.LeftHand))
+        {
+            hapticAction.Execute(0, 1, 150, 75, SteamVR_Input_Sources.LeftHand);
+            Debug.Log("haptic feedback from pressing trackpad");
+        }
+    }
+}
+
+//https://www.youtube.com/watch?v=lOaR-cUGzkc
+
